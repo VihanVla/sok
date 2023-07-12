58aba6a33d35a7b30ef0beb3f2eaeba7de08eb8f
VihanVla <111562310+VihanVla@users.noreply.github.com>
2023-07-12 10:56:25 +0330

    debugging and tested by online networks

--- port/portScan.py
+++ port/portScan.py
@@ -1,23 +1,24 @@
+from Input import allinputs
 import socket
 Portlist = []
 s = None
 
 def checkPort(ip_address, port):
 
-    def PortInput():
-        n = None 
-        while n != '-1':
-            n = input("Give me your port number: ")
-            Portlist.append(n)    
-            print(Portlist)
-    print(f"trying for port {port}")
+    n = None 
+    while n != '-1':
+        n = input("Give me your port number: ")
+        Portlist.append(int(n))    
+        print(Portlist)
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         for port in Portlist:
-            s.connect((ip_address, port))
-            print(f"{ip_address}:{port} online")
+                print(f"trying for port {port}")
+                s.connect((ip_address, port))
+                print(f"{ip_address} : {port} online")
     except Exception as e:
-        print(f"{ip_address}:{port} offline  {e}")
+        print(f"{ip_address} : {port} offline")
     finally:
         s.close()
